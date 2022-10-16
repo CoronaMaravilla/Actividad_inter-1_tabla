e7692e9fad7a43825ba2bef18db23a2daef83f20
CoronaMaravilla <112290836+CoronaMaravilla@users.noreply.github.com>
2022-10-15 19:45:08 -0600

    Ana Silvia Corona Maravilla 61-1329-2019
    <3

---
+++ mi tablaxd_Ana_corona_61-1329-2019/js/animacion.js
@@ -0,0 +1,97 @@
+var rs = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/recetaespa.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var rg = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/recetagalle.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var rm = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/recetaman.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var rl = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/recetalasa.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+
+
+
+
+
+
+var is = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/i espa.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var ig = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/i galletas.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var im = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/i manznas.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var il = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/i lasaña.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+
+var fav = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/favo.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+
+var descar = function(){
+
+    Swal.fire({
+        imageUrl: 'svg/guarda.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
